cb7f15f49e3507ced09b42170bc7791301ec29b9
amitabha ghosh <amitabhaghosh197@gmail.com>
2015-09-20 15:40:30 +0530

    index

---
+++ firstfiles/js/app.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var myApp = angular.module('myApp',[]);
+myApp.controller('MyController',function($scope){
+    $scope.clock = {
+        now: new Date()
+    };
+    var updateClock = function(){
+        $scope.clock.now = new Date();
+    };
+    setInterval( function(){
+        $scope.$apply(updateClock);
+    },1000);
+    updateClock();
+});
+
+var app = angular.module('Name',[]);
+
+app.controller('ChangeName',function($scope){
+     $scope.name = 'World';
+});
+
+angular.bootstrap(document, ['myApp','Name']);
